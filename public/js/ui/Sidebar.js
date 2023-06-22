b7c6ab8b8cfbfa567e25ba4d7e0d808a220eb8a5
lioness1741 <114001429+lioness1741@users.noreply.github.com>
2023-06-22 14:39:26 +0500

    ready
    ready

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -7,7 +7,7 @@ class Sidebar {
   /**
    * Запускает initAuthLinks и initToggleButton
    * */
-  static init() {
+   static init() {
     this.initAuthLinks();
     this.initToggleButton();
   }
@@ -18,7 +18,13 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
+    const sidebarToggle = document.querySelector(".sidebar-toggle");
+    const sidebarMini = document.querySelector("body");
 
+    sidebarToggle.addEventListener("click", event => {
+      sidebarMini.className.add = "sidebar-open";
+      sidebarMini.className.add = "sidebar-collapse";
+    });
   }
 
   /**
@@ -29,6 +35,23 @@ class Sidebar {
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
-
+    const register = document.querySelector(".menu-item_register");
+    register.onclick = function() {
+      App.getModal("register").open();
+    };
+    const login = document.querySelector(".menu-item_login");
+    login.onclick = function() {
+      App.getModal("login").open();
+    };
+    
+    const logout = document.querySelector(".menu-item_logout");
+      logout.onclick = function() {
+      let callback = function(error, response) {
+        if (response.success == true) {
+          App.setState('init');
+        }
+      };
+    User.logout(callback);
+      };
   }
 }
